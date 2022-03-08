00279dc8872d64356ac1584f889d55ae1f1ba421
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-03-08 17:19:30 -0300

    rutas del producto

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const addProductRouter = require("./addProduct");
+const findProductRouter = require("./findProduct");
+const productRouter = require("./product");
+const modifyProductRouter = require("./modifyProduct");
+const deleteProductRouter = require("./deleteProduct");
 
+app.use("/addProduct", addProductRouter);
+app.use("/findProduct", findProductRouter);
+app.use("/product", productRouter);
+app.use("/modifyProduct", modifyProductRouter);
+app.use("/deleteProduct", deleteProductRouter);
 
 module.exports = router;
