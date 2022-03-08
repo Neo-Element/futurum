205ea010ca4ddcd83175f10798ceb2ea109e8dee
Alexis Urquiza <urquizaalexis22@gmail.com>
2022-03-08 15:13:49 -0300

    boilerplate

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,4 @@
 const express = require("express");
 const router = express.Router();
-// const productRouter = require("./products");
-// const findProductRouter = require("./findProduct");
-// const addProductRouter = require("./addProduct");
-
-// router.use("/products", productRouter);
-// router.use("/findProduct", findProductRouter);
-// router.use("/addProduct", addProductRouter);
 
 module.exports = router;
