7031be28859f35c9d46bad206ea099cd992a82bf
Tomas13d <tomas.demobio@gmail.com>
2022-03-08 17:59:24 -0300

    rutas Card terminadas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const routerShoppingCart = require("./shoppingCart");
+
+
+router.use("/shoppingCard", routerShoppingCart);
 
 
 module.exports = router;
