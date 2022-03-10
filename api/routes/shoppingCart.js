6121b235561396d46a669577bbafada49376da42
marisol <salgqdorocio23@gamil.com>
2022-03-10 15:57:48 -0300

    carrito base quilombo

--- api/routes/shoppingCart.js
+++ api/routes/shoppingCart.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const {Users} = require("../models") // correct routes
-
+const {Products}= require("../models")
 
 //add to shopping card recibe a traves de req.body. id del usuario y el arreglo con los id de los productos [12 , 13 ...]
 router.patch("/add", (req,res,next)=>{
@@ -10,6 +10,29 @@ router.patch("/add", (req,res,next)=>{
     }})
     .catch(next)
   })
+
+ router.get("/:id/all", (req, res)=>{
+
+   Users.findOne({
+       where:{id: req.params.id }
+   })
+   .then((user)=>{
+       const arr= user.cart;
+       ///[3,20,25]}}}
+ arr.map((product)=>{
+      Products.findOne({
+           where:{
+              id: product 
+           }
+       })
+
+       .then((product)=>product.data)})
+       return user.cart=arr
+   }).then((user)=> res.send(user).status(200))
+    .catch((err)=> console.log(err))
+   })
+   
+
 
 router.patch("/remove", (req,res,next)=>{
     Users.update(req.body, {where: {
