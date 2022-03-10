5484247945de9e1e2f07f3c27bb81048e62a8d17
Tomas13d <tomas.demobio@gmail.com>
2022-03-10 16:01:58 -0300

    utlimos cambios

--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,40 +1,41 @@
 import CardRange from "../commons/CardRange";
 import Reviews from "../commons/Reviews";
 
+
 //RECIBE UN CURSO EN PARTICULAR
-const SingleCourse = ({ sinlgeCourse }) => {
+const SingleCourse = ({ course }) => {
   return (
     <div className="containerSingle">
       <div className="singleCard">
         <div className="cardTitle">
           <div className="colorBar"> </div>
-          <h5>{`${sinlgeCourse.duration} hours`}</h5>
+          <h5>{`${course.duration} hours`}</h5>
         </div>
         <div className="bodyCard">
           <div className="half">
             <div className="text">
-              <h1>{sinlgeCourse.productName}</h1>
-              <p className="sub">{sinlgeCourse.category}</p>
+              <h1>{course.productName}</h1>
+              <p className="sub">{course.category}</p>
               <hr></hr>
             </div>
             <div class="image">
-              <img src={sinlgeCourse.image} alt="logo" />
+              <img src={course.image} alt="logo" />
             </div>
           </div>
           <div className="half">
             <div className="description">
               <h3>Overview</h3>
-              <p>{sinlgeCourse.overview}</p>
+              <p>{course.overview}</p>
             </div>
             <span className="stock">
               <i className="fa-solid fa-eye"></i>Calification
             </span>
             <div className="calification">
-              <CardRange range={sinlgeCourse.ranking} />
-              <span>{` ${sinlgeCourse.students} students`}</span>
+              <CardRange range={course.ranking} />
+              <span>{` ${course.students} students`}</span>
             </div>
             <div className="priceAndAdd">
-              <p className="price">{`Price: $ ${sinlgeCourse.price}`}</p>
+              <p className="price">{`Price: $ ${course.price}`}</p>
               <button className="btn btn-dark btn-lg btnAdd">
                 <i class="fa-solid fa-cart-arrow-down"></i> Add to Cart
               </button>
@@ -44,12 +45,12 @@ const SingleCourse = ({ sinlgeCourse }) => {
       </div>
       <div className="reviewsCard">
         <h2>Testimonials</h2>
-        <Reviews review={sinlgeCourse.review} />
+        <Reviews review={course.review} />
       </div>
       <hr className="hrDown"></hr>
       <div className="bodyInfo">
         <h2>Requirements</h2>
-        <p>{sinlgeCourse.requirements[0]}</p>
+        <p>{course.requirements[0]}</p>
       </div>
     </div>
   );
