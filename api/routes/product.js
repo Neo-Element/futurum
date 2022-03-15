af6130d6fa5e06ca1130883865dad4651c644ef6
marisol <salgqdorocio23@gamil.com>
2022-03-15 13:13:56 -0300

    comentario añadido

--- api/routes/product.js
+++ api/routes/product.js
@@ -3,6 +3,11 @@ const productRouter = express.Router();
 const Products = require("../models/Products"); //REVISAR QUE COINCIDA CON LA RUTA CORRESPONDIENTE
 const fakeData = require("../utils/fakeData")
 
+
+
+//para que se llene la tabla intermedia de product_category que le setee una categoria a un producto 
+
+
 productRouter.get("/", (req, res) => {
   // Products.findAll()
   //   .then((products) => (products ? res.json(products) : res.sendStatus(404)))
