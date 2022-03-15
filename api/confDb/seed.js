4568e069f3cf3814a74d2a35a7622714fed729c9
marisol <salgqdorocio23@gamil.com>
2022-03-15 11:08:12 -0300

    sedeado

--- api/confDb/seed.js
+++ api/confDb/seed.js
@@ -5,11 +5,10 @@ const {products, reviews, users, categories, payments, carts} = require('../util
 
 
 
-
+const {Categories}=require('../models')
+const {Payments}=require('../models')
 const {Products} = require('../models')
 const {Users}=require('../models')
-const {Payments}=require('../models')
-const {Categories}=require('../models')
 const {Reviews}=require('../models')
 const {Carts}=require('../models')
 
@@ -18,10 +17,10 @@ const db= require('./index')
 
 
 db.sync({force:true}).then(()=>{ 
+    Categories.bulkCreate(categories)
+    Payments.bulkCreate(payments)
     Products.bulkCreate(products)
     Users.bulkCreate(users)
-    Payments.bulkCreate(payments)
-    Categories.bulkCreate(categories)
     Reviews.bulkCreate(reviews)
     Carts.bulkCreate(carts)
 
