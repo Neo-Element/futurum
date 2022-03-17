15e6a4686ba23fd7105e2fea5dee841031390100
marisol <salgqdorocio23@gamil.com>
2022-03-17 19:32:03 -0300

    rutas reviews

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const { Reviews } = require("../models");
 const reviewsRouter = express.Router();
-
+const ReviewController= require("../controllers/reviewsCrontroller")
 //================================================== DE ESTAS RUTAS PODEMOS SACAR LAS REVIEWS Y LOS VOTOS ======================================
-reviewsRouter.get("/all", (req, res) => {
-  Reviews.findAll()
-    .then((reviews) => (reviews ? res.json(reviews) : res.sendStatus(404)))
-    .catch((err) => console.error(err));
-});
+reviewsRouter.get("/all", ReviewController.getAll);
 
-reviewsRouter.get("/:productId", (req, res) => {
-  Reviews.findOne({
-    where: {
-      productId: req.params.productId,
-    },
-  })
-    .then((review) => (review ? res.json(review) : res.sendStatus(404)))
-    .catch((err) => console.error(err));
-});
+reviewsRouter.get("/:productId", ReviewController.getOne);
 
-reviewsRouter.post("/new", (req, res) => {
-  Reviews.create(req.body)
-    .then(() => res.sendStatus(201))
-    .catch((err) => console.error(err));
-});
+reviewsRouter.post("/new", ReviewController.newReview);
 
 module.exports = reviewsRouter;
