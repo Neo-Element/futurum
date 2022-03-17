97e7ebf3e1b8a6b10715176a78c32587e5fa085f
marisol <salgqdorocio23@gamil.com>
2022-03-17 19:30:29 -0300

    rutas categorias

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,52 +1,16 @@
 const express = require("express");
 const categoriesRouter = express.Router();
-const {Categories,Products } = require("../models"); //EN CASO QUE CATEGORÍAS SEA UNA TABLA
+const CategoriesController= require("../controllers/categoriesControllers")
 
 
-categoriesRouter.get("/", (req, res, next) => {
-  Categories.findAll()
-    .then((categories) =>
-      categories ? res.json(categories) : res.sendStatus(404)
-    )
-    .catch((err) => console.log(err));
-});
+categoriesRouter.get("/", CategoriesController.getAll);
 
-categoriesRouter.get("/:id", (req, res, next) => {
-  Categories.findOne({ where: { id: req.params.id } })
-    .then((category) => {
-      res.send(category)})
-    .catch(next);
-});
+categoriesRouter.get("/:id", CategoriesController.getOne);
 
-categoriesRouter.post("/new", (req, res) => {
-  Categories.create(req.body)
-    .then((category) => {
-      res.status(201).json(category);
-    })
-    .catch((err) => console.err(err));
-});
+categoriesRouter.post("/new", CategoriesController.newCategory);
 
-categoriesRouter.delete("/:id", (req, res) => {
-  Categories.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then(() => res.sendStatus(202))
-    .catch((err) => console.log(err));
-});
+categoriesRouter.delete("/:id", CategoriesController.deleteCategory);
 
-categoriesRouter.put("/:id", (req, res) => {
-  Categories.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-    returning: true,
-    plain: true,
-  }).then((result) => {
-    const category = result[1];
-    res.status(201).json(category);
-  });
-});
+categoriesRouter.put("/:id", CategoriesController.updateCategory);
 
 module.exports = categoriesRouter;
