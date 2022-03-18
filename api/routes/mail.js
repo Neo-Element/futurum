689b9632783de51e8ca3cf00d2df518c06e917f4
VirClancy <virginiaclancy@gmail.com>
2022-03-18 00:09:29 -0300

    creacion nodemailer

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+//const nodemailer = require("nodemailer");
+//const {google} = require("googLeapis");
 
 router.post("/", (req, res) =>{
     console.log("ESTO ES REQ.BODY", req.body)
 })
+
+
 
 module.exports = router;
